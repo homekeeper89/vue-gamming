6a7364febaece9e1f1d8912661268577cfbe3698
김장훈 <homekeeper@gdnet.co.kr>
2019-10-31 19:23:52 +0900

    [new] 웹팩 사용방법 추가중
    웹팩이 왜 사용되는지, 이제야 좀 알겠네. 아예 몰랐던 내용

---
+++ num_baseball/main.js
@@ -0,0 +1,4 @@
+import Vue from 'vue'; // package.js에서 vue를 가져옴
+import NumberBaseball from './numberbaseball.vue' // 얘는 js가 아니라서 rules 설정해야함
+
+new Vue().$mount('#root')
---
+++ num_baseball/numberbaseball.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>result</h1>
+    <input v-model="value">
+    <form v-on:submit="onSubmit"></form>
+    <button>입력</button>
+    <div> 시도 {{}}</div>
+  </div>
+</template>
+<script>
+  export default {
+    data(){
+      return{
+        value:'',
+        result:''
+      }
+    },
+    methods:{
+      onSubmit(e){
+        e.preventDefault();
+      }
+    }
+  }
+</script>
+<style>
+
+</style>
