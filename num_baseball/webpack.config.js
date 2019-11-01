1e3fdca4a74887aa2c45cd9765400b6fdaa73ce3
김장훈 <homekeeper@gdnet.co.kr>
2019-11-01 18:22:23 +0900

    [fix] webpack 설정 추가

--- num_baseball/webpack.config.js
+++ num_baseball/webpack.config.js
@@ -1,15 +1,26 @@
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 const path = require('path');
+// js로 인식 안되는 애들은 loader를 설치해서 불러와줘야한다.
+// vue는 vue-loader로 가져오고 있음
 
 module.exports = {
+  mode:'development',
+  devtool:'eval',
+  resolve:{
+    extensions:['.js', '.vue'],
+  },
   entry:{
     app:path.join(__dirname, 'main.js')
   },
   module:{
     rules:[{
-
+      test:/\.vue$/,
+      use:'vue-loader',
     }]
   },
-  plugins:[],
+  plugins:[
+    new VueLoaderPlugin(),
+  ],
   output:{
     filename:'[name].js',
     path:path.join(__dirname, 'dist')
