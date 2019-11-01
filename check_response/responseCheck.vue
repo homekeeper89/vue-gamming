f8e0ec4ef4476650763a954e75ce0c2e5e95ad7b
김장훈 <homekeeper@gdnet.co.kr>
2019-11-01 18:50:28 +0900

    [new] 반응속도 게임 문제 환경 추가
    class를 동적으로 하려면 v-bind:class="data변수" 로 하거나
    :class="data변수" 로 하면 된다

---
+++ check_response/responseCheck.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div id="screen" :class='state'>{{message}}</div>
+    <div>
+      <div>평균 시간:{{}}</div>
+      <button @click="onReset">리셋</button>
+    </div>
+  </div>
+</template>
+<script>
+  export default { // export default 를 해서 main.js 에서 import from 으로 가져올 수 가 있다
+    data(){
+      return{
+        result:[],
+        state:'waiting',
+        message:'클릭해서 시작하세요'
+      }
+    },
+    methods:{
+      onReset(){
+        
+      }
+    }
+  }
+</script>
+<style scoped>
+  #screen{
+    width:300px;
+    height:200px;
+    text-align:center;
+    user-select:none;
+  }
+  #screen.waiting{
+    background-color:aqua;
+  }
+  #screen.ready{
+    background-color:red;
+  }
+  #screen.now{
+    background-color:greenyellow
+  }
+</style>
