54b99132899884e4066ecbb435ce838ff6e21722
김장훈 <homekeeper@gdnet.co.kr>
2019-11-01 19:00:47 +0900

    [new] webpack-dev-server 사용하려면 publicPaht를 추가해야함

--- check_response/webpack.config.js
+++ check_response/webpack.config.js
@@ -2,7 +2,6 @@ const VueLoaderPlugin = require('vue-loader/lib/plugin')
 const path = require('path');
 // js로 인식 안되는 애들은 loader를 설치해서 불러와줘야한다.
 // vue는 vue-loader로 가져오고 있음
-
 module.exports = {
   mode:'development',
   devtool:'eval',
@@ -26,6 +25,7 @@ module.exports = {
   ],
   output:{
     filename:'[name].js',
-    path:path.join(__dirname, 'dist')
+    path:path.join(__dirname, 'dist'),
+    publicPath:'/dist',
   },
 }
