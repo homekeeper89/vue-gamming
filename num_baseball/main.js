b293b65c47a327327cc4c8b88024c70ad74bff57
김장훈 <homekeeper@gdnet.co.kr>
2019-11-01 18:16:50 +0900

    [fix] main.js 수정
    주석 설명 추가함. import from 은 export default와 한쌍이다

--- num_baseball/main.js
+++ num_baseball/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'; // package.js에서 vue를 가져옴
-import NumberBaseball from './numberbaseball.vue' // 얘는 js가 아니라서 rules 설정해야함
-
-new Vue().$mount('#root')
+import NumberBaseball from './numberbaseball' // 얘는 js가 아니라서 rules 설정해야함
+// nodeㅜ 환경은 require, vue환경운 import from
+// webpack에 extenstions에 vue가 추가되서 numberbaseball 로 
+new Vue(NumberBaseball).$mount('#root')
