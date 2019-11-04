a99e045d54f0e724ed83b9f0743f838aa257ee4e
김장훈 <homekeeper@gdnet.co.kr>
2019-11-04 17:49:14 +0900

    [new] vue lifecycle created > mounted > updated > destroyed
    before가 붙으면 해당 시점 이전에도 가능하지만 일단 기본적으로 4개로 이해

--- rock_play/rockPlay.vue
+++ rock_play/rockPlay.vue
@@ -8,6 +8,7 @@
     </div>
     <div>{{result}}</div>
     <div>현재 {{score}} 점</div>
+    
   </div>
 </template>
 <script>
@@ -16,6 +17,7 @@
     가위:'-142px',
     보:'-284px'
   }
+  let interval = null;
   export default { // export default 를 해서 main.js 에서 import from 으로 가져올 수 가 있다
     data(){
       return{
@@ -33,6 +35,31 @@
       onClickButton(choice){
 
       }
+    },
+    created(){ // 보여지긴 하지만 화면 나타나기 전 
+      console.log('created')
+    },
+    mounted(){ // 화면 나타난 후
+      // console.log('mounted')
+      interval = setInterval(()=>{
+        if(this.imgCoord === rspCoords.바위){
+          this.imgCoord = rspCoords.가위
+        }else if(this.imgCoord === rspCoords.가위){
+          this.imgCoord = rspCoords.보
+        }else if(this.imgCoord === rspCoords.보){
+          this.imgCoord = rspCoords.바위
+        }
+      }, 100)
+      // 위 인터벌은 계속 살아 있는다. 컴포넌트가 사라져도 존재하므로 => 메모리 누수가 발생함
+    },
+    updated(){ // 화면 내용물이 바뀌면
+      // console.log('updated')
+    },
+    beforeDestroy(){ 
+      clearInterval(interval)
+    },
+    destroyed(){ // 화면에 있다가 사라지면
+      // console.log('destroyed')
     }
   }
 </script>
