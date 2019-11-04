fa5a5d41a07427cdd7fccf39f320c38df97865bb
김장훈 <homekeeper@gdnet.co.kr>
2019-11-04 22:15:09 +0900

    [new] 로또 생성기 환경
    로또를 생성할때 로또볼들이 나타나는데, 이들은 숫자와 색상이 다를뿐 나머지는 같다.
    이런 경우 이를 컴포넌트화 시키면 된다

---
+++ lotto/LottoGenerator.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <div>당첨숫자</div>
+    <div id="결과창">
+      <lotto-ball v-for="ball in winBalls" :key="ball"></lotto-ball>
+    </div>
+    <div>보너스</div>
+    <lotto-ball v-if="bonus"></lotto-ball>
+    <button v-if='redo'>한번더</button>
+  </div>
+</template>
+<script>
+  export default{
+    data(){
+      return{
+        redo:false,
+        winBalls:[],
+        bonus:null,
+      }
+    },
+    computed:{
+
+    },
+    methods:{
+
+    },
+    mounted(){
+
+    },
+    watch:{
+
+    }
+  }
+</script>
+<style scoped>
+
+</style>
