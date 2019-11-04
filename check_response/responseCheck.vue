77f823036f4c27fdd5141ed0dcab8e3eb2e09d41
김장훈 <homekeeper@gdnet.co.kr>
2019-11-04 16:00:57 +0900

    [fix] 게임 완성
    게임의 속도 체크는 데이터가 아니라 스크립트 변수로 활용
    데이터로 사용되는 것은 화면에 보이는 것만이라는 것 명심
    
    result.reduce((a,b) => a+b, 0), reduce 함수 사용법

--- check_response/responseCheck.vue
+++ check_response/responseCheck.vue
@@ -2,12 +2,15 @@
   <div>
     <div id="screen" :class='state' @click="onClickScreen">{{message}}</div>
     <div>
-      <div>평균 시간:{{}}</div>
+      <div>평균 시간:{{result.reduce((a,c) => a+c, 0) / result.length || 0}}ms</div>
       <button @click="onReset">리셋</button>
     </div>
   </div>
 </template>
 <script>
+  let startTime = 0; 
+  let endTime = 0;
+  let timeOut=null;
   export default { // export default 를 해서 main.js 에서 import from 으로 가져올 수 가 있다
     data(){
       return{
@@ -18,15 +21,26 @@
     },
     methods:{
       onReset(){
-        
+        this.result = []
       },
       onClickScreen(){
         if(this.state ==='waiting'){
           this.state = 'ready';
+          this.message = '초록색이 되면 클릭하세요'
+          setTimeout(()=>{
+            this.state = 'now'
+            this.message="지금 클릭"
+            startTime = new Date();
+          }, Math.floor(Math.random() * 1000 + 2000))
         }else if(this.state === 'ready'){
-          this.state = 'now'
-        }else if(this.state === 'now'){
+          clearTimeout(timeOut)
           this.state = 'waiting'
+          this.message = '너무 성급하시군요 초록색이 된 후 클릭하세요'
+        }else if(this.state === 'now'){
+          endTime = new Date();
+          this.state = 'waiting'
+          this.message = '클릭해서 시작하세요'
+          this.result.push(endTime - startTime);
         }
       }
     }
