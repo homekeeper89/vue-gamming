cbfa0943365387dbddde51fa69aa7a7b955f9144
김장훈 <homekeeper@gdnet.co.kr>
2019-11-04 22:53:52 +0900

    [fix] clearTimeout 추가
    setTimeout은 메모리 누수가 발생 가능 > 화면에서 사라졌어도 계속 켜있을 수 있으므로
    그렇기에 beforeDestory에서 꼭 clearTimeout을 해줘야한다

--- lotto/LottoGenerator.vue
+++ lotto/LottoGenerator.vue
@@ -2,19 +2,20 @@
   <div>
     <div>당첨숫자</div>
     <div id="결과창">
-      <lotto-ball v-for="ball in winBalls" :key="ball" number="5"></lotto-ball>
+      <lotto-ball v-for="ball in winBalls" :key="ball" :number="ball"></lotto-ball>
     </div>
     <div>보너스</div>
-    <lotto-ball v-if="bonus"></lotto-ball>
-    <button v-if='redo'>한번더</button>
+    <lotto-ball v-if="bonus" :number="bonus"></lotto-ball>
+    <button v-if='redo' @click="onClickRedo">한번더</button>
   </div>
 </template>
 <script>
   import LottoBall from './LottoBall';
-  const getWinNumbers = () =>{
+  const timeOuts = [];
+  function getWinNumbers(){
     const candidate = Array(45).fill().map((v,i) => i+1)
     const shuffle = [];
-    while(candiate.length > 0){
+    while(candidate.length > 0){
       shuffle.push(candidate.splice(Math.floor(Math.random() * candidate.length), 1)[0])
     }
     const bonusNumber = shuffle[shuffle.length -1];
@@ -37,13 +38,35 @@
 
     },
     methods:{
-
+      onClickRedo(){
+        this.winNumbers = getWinNumbers();
+        this.winBalls = [];
+        this.bonus = null;
+        this.redo = false;
+        this.showBalls()
+      },
+      showBalls(){
+        for(let i =0; i<this.winNumbers.length-1; i++){
+        timeOuts[i] = setTimeout(()=>{
+          this.winBalls.push(this.winNumbers[i])
+        }, (i+1) * 1000)
+      }
+      timeOuts[6] = setTimeout(()=>{
+        this.bonus = this.winNumbers[6];
+        this.redo = true
+      }, 7000)
+      }
     },
     mounted(){
-
+      this.showBalls()
     },
     watch:{
 
+    },
+    beforeDestroy(){
+      timeOuts.forEach((t)=>{
+        clearTimeout(t)
+      })
     }
   }
 </script>
