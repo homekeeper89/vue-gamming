20b1e1df061ffd99ccbd8bc409e9b04dfbdc6a54
김장훈 <homekeeper@gdnet.co.kr>
2019-11-04 18:05:56 +0900

    [fix] 가위바위보 게임 완성
    Object.entries(rspCoords) 를 하게 되면 [key, value], [key, value].. 이런식으로 나온다

--- rock_play/rockPlay.vue
+++ rock_play/rockPlay.vue
@@ -17,6 +17,16 @@
     가위:'-142px',
     보:'-284px'
   }
+  const scores = {
+    가위:1,
+    바위:0,
+    보:-1,
+  }
+  const computerChoice =(imgCoord) =>{ // [key,value] 이렇게 나옴
+    return Object.entries(rspCoords).find(function(v){
+      return v[1] === imgCoord;
+    })[0]
+  }
   let interval = null;
   export default { // export default 를 해서 main.js 에서 import from 으로 가져올 수 가 있다
     data(){
@@ -32,16 +42,8 @@
       }
     },
     methods:{
-      onClickButton(choice){
-
-      }
-    },
-    created(){ // 보여지긴 하지만 화면 나타나기 전 
-      console.log('created')
-    },
-    mounted(){ // 화면 나타난 후
-      // console.log('mounted')
-      interval = setInterval(()=>{
+      changeHand(){
+        interval = setInterval(()=>{
         if(this.imgCoord === rspCoords.바위){
           this.imgCoord = rspCoords.가위
         }else if(this.imgCoord === rspCoords.가위){
@@ -50,6 +52,32 @@
           this.imgCoord = rspCoords.바위
         }
       }, 100)
+      },
+      onClickButton(choice){
+        clearInterval(interval); // 손가락 움직이는게 멈춰야 하니까
+        const myScore = scores[choice]
+        const cpuScored = scores[computerChoice(this.imgCoord)]
+        const diff = myScore - cpuScored
+        if(diff === 0){
+          this.result = '비겼습니다'
+        }else if([-1, 2].includes(diff)){
+          this.result = '이겼습니다'
+          this.score += 1;
+        }else{
+          this.result = '졌습니다'
+          this.score -= 1;
+        }
+        setTimeout(()=>{
+          this.changeHand()
+        }, 1000)
+      }
+    },
+    created(){ // 보여지긴 하지만 화면 나타나기 전 
+      console.log('created')
+    },
+    mounted(){ // 화면 나타난 후
+      // console.log('mounted')
+      this.changeHand()
       // 위 인터벌은 계속 살아 있는다. 컴포넌트가 사라져도 존재하므로 => 메모리 누수가 발생함
     },
     updated(){ // 화면 내용물이 바뀌면
