1c9695ff1fae8bb8559cb80745098fcc0fb75559
김장훈 <homekeeper@gdnet.co.kr>
2019-11-04 17:33:40 +0900

    [fix] computed obj 사용
    계속 바뀌어야 하는 부분이므로 computed 로 추가

--- rock_play/rockPlay.vue
+++ rock_play/rockPlay.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div id="computer" :style="{background:`url(https://en.pimg.jp/023/182/267/1/23182267.jpg) 0 0`}"></div>
+    <div id="computer" :style="computedStyleObj"></div>
     <div>
       <button @click="onClickButton('바위')">바위</button>
       <button @click="onClickButton('가위')">가위</button>
@@ -11,15 +11,23 @@
   </div>
 </template>
 <script>
+  const rspCoords = {
+    바위 : 0,
+    가위:'-142px',
+    보:'-284px'
+  }
   export default { // export default 를 해서 main.js 에서 import from 으로 가져올 수 가 있다
     data(){
       return{
+        imgCoord:rspCoords.바위,
         result:'',
         score:0, 
       }
     },
     computed:{ // 일반 data를 가공해서 쓸때 computed를 사용한다
-      
+      computedStyleObj(){
+        return {background:`url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${this.imgCoord} 0`}
+      }
     },
     methods:{
       onClickButton(choice){
