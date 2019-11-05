fc63e009c6e4ab41476b26082e45bc199f17967c
matthew <homekeeper89@gmail.com>
2019-11-05 21:29:57 +0900

    [fix] 잊혀진 애들 추가함

---
+++ lotto/LottoBall.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="ball" :style="{background}">{{number}}</div>
+</template>
+<script>
+export default {
+  name:'LottoBall',
+  props:{
+    number:Number,
+  },
+  computed:{
+    background(){
+      let background;
+      if(this.number < 10){
+        background ="red"
+      }else if(this.number <=20){
+        background ="orange"
+      }else if(this.number <=30){
+        background ="yellow"
+      }else{
+        background ="green"
+      }
+    return background
+    }
+  },
+  data(){
+    let background;
+    return{
+
+    }
+  }
+}
+</script>
+<style scoped>
+  .ball{
+    display:inline-block;
+    border:1px solid black;
+    border-radius:20px;
+    width:40px;
+    height:40px;
+    line-height:40px;
+    font-size:20px;
+    text-align:center;
+    margin-right:20px;
+  }
+</style>
