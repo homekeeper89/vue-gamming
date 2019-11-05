f2ee092df78d00aabcf40b34ff4b14ed5471c4c7
matthew <homekeeper89@gmail.com>
2019-11-05 23:29:05 +0900

    [new] getters 사용
    기본적으로 state, mutation이랑 동일
    ...mapGetters가 존재
    접근은 this.$store.getters로

--- tic_vuex/TableComponent.vue
+++ tic_vuex/TableComponent.vue
@@ -12,6 +12,9 @@ export default {
   computed:{
     tableData(){
       return this.$store.state.tableData;
+    },
+    turnMessage(){ // ...mapGetters가 존재함
+      return this.$store.getters.turnMessage
     }
   }
   // props:{
