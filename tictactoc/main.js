1f237e46611c7c852fdb717f0664d212ed324641
matthew <homekeeper89@gmail.com>
2019-11-05 15:14:02 +0900

    [fix] root 환경 수정

--- tictactoc/main.js
+++ tictactoc/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'; // package.js에서 vue를 가져옴
-import LottoGenerator from './LottoGenerator' // 얘는 js가 아니라서 rules 설정해야함
+import TicTacToc from './TicTacToc' // 얘는 js가 아니라서 rules 설정해야함
 // nodeㅜ 환경은 require, vue환경운 import from
 // webpack에 extenstions에 vue가 추가되서 numberbaseball 로 
-new Vue(LottoGenerator).$mount('#root')
+new Vue(TicTacToc).$mount('#root')
