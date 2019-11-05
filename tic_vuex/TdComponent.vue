ecce830e23f1522c4afe4e14860160a71eee616d
matthew <homekeeper89@gmail.com>
2019-11-05 23:28:28 +0900

    [new] mapState 추가

--- tic_vuex/TdComponent.vue
+++ tic_vuex/TdComponent.vue
@@ -3,6 +3,7 @@
 </template>
 <script>
 import {CLICK_CELL,SET_WINNER, RESET_GAME, CHANGE_TURN, NO_WINNER} from './store'
+import {mapState} from 'vuex'
 export default{
   props:{
     // cellData:String,
@@ -10,15 +11,22 @@ export default{
     rowIndex:Number,
   },
   computed:{ // vuex 사용할려면 반듯이 computed에 연결해줘야한다
-    cellData(){
-      return this.$store.state.tableData[this.rowIndex][this.cellIndex]
-    },
-    tableData(){
-      return this.$store.state.tableData;
-    }, 
-    turn(){
-      return this.$store.state.turn
-    }
+    ...mapState({
+      tableData:state => state.tableData,
+      turn : state => state.turn,
+      cellData(state){
+        return state.tableData[this.rowIndex][this.cellIndex]
+      }
+    })
+    // cellData(){
+    //   return this.$store.state.tableData[this.rowIndex][this.cellIndex]
+    // },
+    // tableData(){
+    //   return this.$store.state.tableData;
+    // }, 
+    // turn(){
+    //   return this.$store.state.turn
+    // }
   },
   methods:{
     onClickTd(){
