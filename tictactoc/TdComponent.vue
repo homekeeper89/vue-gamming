236ff87162730f374ac38b9a879b770ba667c070
matthew <homekeeper89@gmail.com>
2019-11-05 15:28:27 +0900

    [new]
    vue에서 단순 변수의 data 변화는 금새 화면에 렌더 된다
    단 리스트의 변화에 있어서 인덱스로 접근하게 될 경우 불가하다
    (push 등의 리스트 함수는 가능)
    그렇기에 이를 해결하기 위해선 vue.$set 또는 this.$set 등을 활용해야한다
    (vue.set 사용시 vue 임포트 해야함)

--- tictactoc/TdComponent.vue
+++ tictactoc/TdComponent.vue
@@ -9,10 +9,15 @@ export default{
     rowIndex:Number,
   },
   methods:{
-    onClickTd(){
-      console.log(this.$root.$data)
-      console.log(this.$parent.$data)
-      this.$root.$data.turn = this.$root.$data.turn ==='O' ? 'X' : 'O'
+    onClickTd(){ // vue에서 리스트의 내부 값을 바꾼다고 화면에 반영되는 것은 아니다.
+      // 자바스크립트의 내부적 한계
+      // 단 push 등을 통해서 바꾸는 것은 가능, 인덱스로 하는건 불가
+      // Vue.set(객체, 인덱스, 바꾸려는 값) 으로 가능함
+      // = this.$set(이하 동일) 
+      // this.$root.$data.tableData[this.rowIndex][this.cellIndex] = this.$root.$data.turn;
+      const rootData = this.$root.$data
+      this.$set(rootData.tableData[this.rowIndex], this.cellIndex, rootData.turn)
+      rootData.turn = rootData.turn ==='O' ? 'X' : 'O'
     }
   }
 }
